34f6f909e21898bf37e5111487c677abe25cc213
dabideee13 <d.e.magno13@gmail.com>
2021-05-09 15:33:31 +0800

    Initial app boilerplate

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ from scipy.integrate import odeint
 
 import plotly
 
-plotly.io.orca.config.executable = '/opt/anaconda3/envs/siyc/bin/orca'
-
 import dash
 import dash_core_components as dcc 
 import dash_html_components as html 
